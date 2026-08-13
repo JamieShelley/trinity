7666e7d78b0904f75fec2e0aac7722f193b462c9
ccp-intern <180460643+ccp-intern@users.noreply.github.com>
2026-08-13 12:32:47 +0200

    formatting

--- trinity/Eve/SpaceObject/EveSpaceObject2_Blue.cpp
+++ trinity/Eve/SpaceObject/EveSpaceObject2_Blue.cpp
@@ -507,29 +507,29 @@ const Be::ClassInfo* EveSpaceObject2::ExposeToBlue()
 			"Get the closest locator in set to target, does not mind about direction of locator.\n"
 			":param position: position of target\n"
 			":param locatorSetName: name of locator set \n" )
-			MAP_METHOD_AND_WRAP(
-				"GetLocatorPositionFromSet",
-				GetLocatorPositionFromSet,
-				"locator position from a set Specified by name\n"
-				":param index: locator index\n"
-				":param inWorldSpace: position of target\n"
-				":param locatorSetName: name of locator set \n" )
-				MAP_METHOD_AND_WRAP(
-					"GetLocatorRotationFromSet",
-					GetLocatorRotationFromSet,
-					"locator rotation in worldspace\n"
-					":param index: locator index\n"
-					":param inWorldSpace: position of target\n"
-					":param locatorSetName: name of locator set \n" )
-					MAP_METHOD_AND_WRAP( "ClearImpactDamage", ClearImpactDamage, "Clear all the impact/damage effects." )
-						MAP_METHOD_AND_WRAP(
-							"CreateImpact",
-							CreateImpact,
-							"debug only\n"
-							":param idx: damage locator index\n"
-							":param direction: incoming damage direction\n"
-							":param lifeTime: effect time\n"
-							":param size: effect size" );
+		MAP_METHOD_AND_WRAP(
+			"GetLocatorPositionFromSet",
+			GetLocatorPositionFromSet,
+			"locator position from a set Specified by name\n"
+			":param index: locator index\n"
+			":param inWorldSpace: position of target\n"
+			":param locatorSetName: name of locator set \n" )
+		MAP_METHOD_AND_WRAP(
+			"GetLocatorRotationFromSet",
+			GetLocatorRotationFromSet,
+			"locator rotation in worldspace\n"
+			":param index: locator index\n"
+			":param inWorldSpace: position of target\n"
+			":param locatorSetName: name of locator set \n" )
+		MAP_METHOD_AND_WRAP( "ClearImpactDamage", ClearImpactDamage, "Clear all the impact/damage effects." )
+		MAP_METHOD_AND_WRAP(
+			"CreateImpact",
+			CreateImpact,
+			"debug only\n"
+			":param idx: damage locator index\n"
+			":param direction: incoming damage direction\n"
+			":param lifeTime: effect time\n"
+			":param size: effect size" );
 		MAP_METHOD_AND_WRAP(
 			"CreateImpactFromPosition",
 			CreateImpactFromPosition,
